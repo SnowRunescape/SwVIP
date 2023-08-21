cc0acc89d5323ed389b3a585e12cb97d794e681f
Bruno Caitano <snowrunescape@snowdev.com.br>
2023-08-20 21:04:50 -0400

    feat: implements base of give vip

--- src/br/com/snowdev/swvip/commands/GiveVip.java
+++ src/br/com/snowdev/swvip/commands/GiveVip.java
@@ -4,13 +4,32 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import br.com.snowdev.swvip.SwVIP;
 import br.com.snowdev.swvip.interfaces.CommandPermissions;
+import br.com.snowdev.swvip.utilities.Messaging;
+import br.com.snowdev.swvip.utilities.ParserNumber;
 
 @CommandPermissions("swvip.command.swvip")
 public class GiveVip implements CommandExecutor
 {
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
     {
-        return false;
+        if (args.length != 3) {
+            String message = "§f/givevip <username> <group> <days>";
+
+            sender.sendMessage(Messaging.format(message, true, false));
+            return false;
+        }
+
+        String username = args[0].toLowerCase();
+        String group = args[1];
+        int days = ParserNumber.parseInt(args[2]);
+
+        if (!SwVIP.instance.giveVIP(player, group, days)) {
+            sender.sendMessage(Messaging.format("Erro ao dar o Vip", true, false));
+            return false;
+        }
+
+        return true;
     }
 }
