6421d67a809af02821aa26abd603bd7721811e5f
Bruno Caitano <snowrunescape@snowdev.com.br>
2023-08-24 19:07:24 -0400

    fix: apenas jogadores podem executar esse comando

--- src/br/com/snowdev/swvip/commands/ChangeVip.java
+++ src/br/com/snowdev/swvip/commands/ChangeVip.java
@@ -3,14 +3,21 @@ package br.com.snowdev.swvip.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import br.com.snowdev.swvip.interfaces.CommandPermissions;
+import br.com.snowdev.swvip.utilities.Messaging;
 
 @CommandPermissions("swvip.command.swvip")
 public class ChangeVip implements CommandExecutor
 {
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
     {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(Messaging.format("error.cmd_only_player", true, true));
+            return false;
+        }
+
         return false;
     }
 }
